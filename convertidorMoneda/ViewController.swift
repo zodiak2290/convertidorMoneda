cc504c03eec84c66dccccba24ef2e51051fd9146
Alberto Martinez <albon_marvel@hotmail.com>
2017-01-05 13:55:30 -0600

    Convertidor moneda

--- convertidorMoneda/ViewController.swift
+++ convertidorMoneda/ViewController.swift
@@ -9,10 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let valDolar: Float = 21.3890018
+    let valEuro: Float = 22.6659252
+    
+    var convertDollar: Float = 0
+    var convertEuro: Float = 0
+    var valorAConvertir : Float = 0
+    
+    @IBOutlet weak var textValorAConvertir: UITextField!
+    @IBOutlet weak var labelResultado: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        showResult(valorResultado:"")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func buttonConvertirDolar(_ sender: Any) {
+        self.convertir(moneda: valDolar)
+    }
+    
+    @IBAction func buttonConvertirEuro(_ sender: Any) {
+        self.convertir(moneda: valEuro)
+    }
+    
+    private func convertir(moneda :Float){
+        valorAConvertir = self.getValorAConvertir()
+        let convert: Float = valorAConvertir / moneda
+        self.showResult(valorResultado: convert)
+    }
+    
+    private func getValorAConvertir() -> Float {
+        if let texto = textValorAConvertir.text {
+            return Float(texto)!
+        }else{
+            return 0
+        }
+    }
+    
+    private func showResult(valorResultado :Any){
+        labelResultado.text = String(describing: valorResultado)
+    }
 }
-
